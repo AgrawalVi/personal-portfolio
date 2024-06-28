8daba8217a5945c30e46b3abbca09c5195356818
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-28 16:57:32 +0200

    edited navbar

--- src/components/aceternity/navbar-menu.tsx
+++ src/components/aceternity/navbar-menu.tsx
@@ -1,122 +1,165 @@
-"use client";
+"use client"
 
-import React from "react";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import Image from "next/image";
+import React from "react"
+import { motion } from "framer-motion"
+import Link from "next/link"
+import Image from "next/image"
 
-const transition = {
-    type: "spring",
-    mass: 0.5,
-    damping: 11.5,
-    stiffness: 100,
-    restDelta: 0.001,
-    restSpeed: 0.001,
-};
+const transitionIn = {
+  type: "spring",
+  mass: 0.5,
+  damping: 11.5,
+  stiffness: 100,
+  restDelta: 0.001,
+  restSpeed: 0.001,
+}
+
+const transitionOut = {
+  type: "spring",
+  mass: 0.7,
+  stiffness: 100,
+  restDelta: 0.001,
+  restSpeed: 0.001,
+}
 
 export const MenuItem = ({
-    setActive,
-    active,
-    item,
-    children,
+  setActive,
+  active,
+  lastActive,
+  setLastActive,
+  item,
+  children,
 }: {
-    setActive: (item: string) => void;
-    active: string | null;
-    item: string;
-    children?: React.ReactNode;
+  setActive: (item: string) => void
+  setLastActive: (item: string) => void
+  active: string | null
+  lastActive: string | null
+  item: string
+  children?: React.ReactNode
 }) => {
-    return (
-        <div onMouseEnter={() => setActive(item)} className="relative ">
-            <motion.p
-                transition={{ duration: 0.3 }}
-                className="cursor-pointer text-white hover:opacity-[0.9]"
-            >
-                {item}
-            </motion.p>
-            {active !== null && (
+  return (
+    <div
+      onMouseEnter={() => setActive(item)}
+      onMouseLeave={() => {}}
+      className="relative"
+    >
+      <motion.p
+        transition={{ duration: 0.3 }}
+        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
+      >
+        {item}
+      </motion.p>
+      {active !== null && (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.85, y: 10 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={transitionIn}
+        >
+          {active === item && (
+            <div className="absolute left-1/2 top-[calc(100%_+_1.2rem)] -translate-x-1/2 transform pt-4">
+              <motion.div
+                transition={transitionIn}
+                layoutId="active" // layoutId ensures smooth animation
+                className="overflow-hidden rounded-2xl border border-black/[0.2] bg-white shadow-xl backdrop-blur-sm dark:border-white/[0.2] dark:bg-black"
+              >
                 <motion.div
-                    initial={{ opacity: 0, scale: 0.85, y: 10 }}
-                    animate={{ opacity: 1, scale: 1, y: 0 }}
-                    transition={transition}
+                  layout // layout ensures smooth animation
+                  className="h-full w-max p-4"
                 >
-                    {active === item && (
-                        <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
-                            <motion.div
-                                transition={transition}
-                                layoutId="active" // layoutId ensures smooth animation
-                                className="bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-white/[0.2] shadow-xl"
-                            >
-                                <motion.div
-                                    layout // layout ensures smooth animation
-                                    className="w-max h-full p-4"
-                                >
-                                    {children}
-                                </motion.div>
-                            </motion.div>
-                        </div>
-                    )}
+                  {children}
                 </motion.div>
-            )}
-        </div>
-    );
-};
+              </motion.div>
+            </div>
+          )}
+        </motion.div>
+      )}
+      {active !== item && lastActive === item ? (
+        <motion.div
+          initial={{ opacity: 1, scale: 1, y: 0 }}
+          animate={{ opacity: 0, scale: 0.85, y: 20 }}
+          transition={transitionOut}
+          className="pointer-events-none"
+        >
+          <div className="absolute left-1/2 top-[calc(100%_+_1.2rem)] -translate-x-1/2 transform pt-4">
+            <motion.div
+              transition={transitionOut}
+              layoutId="active" // layoutId ensures smooth animation
+              className="overflow-hidden rounded-2xl border border-black/[0.2] bg-white shadow-xl backdrop-blur-sm dark:border-white/[0.2] dark:bg-black"
+            >
+              <motion.div
+                layout // layout ensures smooth animation
+                className="h-full w-max p-4"
+              >
+                {children}
+              </motion.div>
+            </motion.div>
+          </div>
+        </motion.div>
+      ) : null}
+    </div>
+  )
+}
 
 export const Menu = ({
-    setActive,
-    children,
+  active,
+  setActive,
+  setLastActive,
+  children,
 }: {
-    setActive: (item: string | null) => void;
-    children: React.ReactNode;
+  active: string | null
+  setActive: (item: string | null) => void
+  setLastActive: (item: string | null) => void
+  children: React.ReactNode
 }) => {
-    return (
-        <nav
-            onMouseLeave={() => setActive(null)} // resets the state
-            className="relative rounded-full border bg-black border-white/[0.2] shadow-input flex justify-center space-x-4 px-8 py-6 "
-        >
-            {children}
-        </nav>
-    );
-};
+  return (
+    <nav
+      onMouseLeave={() => {
+        setLastActive(active)
+        setActive(null)
+      }}
+      className="relative flex items-center justify-center space-x-4 bg-background px-8 py-6"
+    >
+      {children}
+    </nav>
+  )
+}
 
 export const ProductItem = ({
-    title,
-    description,
-    href,
-    src,
+  title,
+  description,
+  href,
+  src,
 }: {
-    title: string;
-    description: string;
-    href: string;
-    src: string;
+  title: string
+  description: string
+  href: string
+  src: string
 }) => {
-    return (
-        <Link href={href} className="flex space-x-2">
-            <Image
-                src={src}
-                width={140}
-                height={70}
-                alt={title}
-                className="flex-shrink-0 rounded-md shadow-2xl"
-            />
-            <div>
-                <h4 className="text-xl font-bold mb-1 text-white">
-                    {title}
-                </h4>
-                <p className="text-sm max-w-[10rem] text-neutral-300">
-                    {description}
-                </p>
-            </div>
-        </Link>
-    );
-};
+  return (
+    <Link href={href} className="flex space-x-2">
+      <Image
+        src={src}
+        width={140}
+        height={70}
+        alt={title}
+        className="flex-shrink-0 rounded-md shadow-2xl"
+      />
+      <div>
+        <h4 className="text-xl font-bold mb-1 text-white">{title}</h4>
+        <p className="text-sm max-w-[10rem] text-neutral-300">{description}</p>
+      </div>
+    </Link>
+  )
+}
 
-export const HoveredLink = ({ children, ...rest }: any) => {
-    return (
-        <Link
-            {...rest}
-            className="text-neutral-200 hover:text-neutral-600 transition-all"
-        >
-            {children}
-        </Link>
-    );
-};
+export const HoveredLink = ({ item, setActive, children, ...rest }: any) => {
+  return (
+    <Link
+      {...rest}
+      className="text-neutral-200 hover:text-neutral-600 transition-all"
+      onMouseEnter={() => setActive(item)}
+    >
+      {children}
+    </Link>
+  )
+}
--- src/components/general/navbar.tsx
+++ src/components/general/navbar.tsx
@@ -11,55 +11,56 @@ import { cn } from "@/lib/utils"
 
 export function Navbar() {
   const [active, setActive] = useState<string | null>(null)
+  const [lastActive, setLastActive] = useState<string | null>(null)
 
   return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 top-2")}>
-      <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Services">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-            <ProductItem
-              title="Algochurn"
-              href="https://algochurn.com"
-              src="/agriculture-hall.jpg"
-              description="Prepare for tech interviews like never before."
-            />
-            <ProductItem
-              title="Tailwind Master Kit"
-              href="https://tailwindmasterkit.com"
-              src="/agriculture-hall.jpg"
-              description="hello"
-            />
-            <ProductItem
-              title="Moonbeam"
-              href="https://gomoonbeam.com"
-              src="/agriculture-hall.jpg"
-              description="Never write from scratch again. Go from idea to blog in minutes."
-            />
-            <ProductItem
-              title="Rogue"
-              href="https://userogue.com"
-              src="/agriculture-hall.jpg"
-              description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-            />
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Pricing">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
-      </Menu>
-    </div>
+    <Menu setActive={setActive} setLastActive={setLastActive} active={active}>
+      <HoveredLink setActive={setActive} item="Home" href="/">
+        Home
+      </HoveredLink>
+      <HoveredLink setActive={setActive} item="About Me" href="/about">
+        About Me
+      </HoveredLink>
+      <MenuItem
+        setActive={setActive}
+        active={active}
+        item="Projects"
+        lastActive={lastActive}
+        setLastActive={setLastActive}
+      >
+        <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+          <ProductItem
+            title="Algochurn"
+            href="https://algochurn.com"
+            src="/agriculture-hall.jpg"
+            description="Prepare for tech interviews like never before."
+          />
+          <ProductItem
+            title="Tailwind Master Kit"
+            href="https://tailwindmasterkit.com"
+            src="/agriculture-hall.jpg"
+            description="hello"
+          />
+          <ProductItem
+            title="Moonbeam"
+            href="https://gomoonbeam.com"
+            src="/agriculture-hall.jpg"
+            description="Never write from scratch again. Go from idea to blog in minutes."
+          />
+          <ProductItem
+            title="Rogue"
+            href="https://userogue.com"
+            src="/agriculture-hall.jpg"
+            description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+          />
+        </div>
+      </MenuItem>
+      <HoveredLink setActive={setActive} item="Blog" href="/Blog">
+        Blog
+      </HoveredLink>
+      <HoveredLink setActive={setActive} item="Contact" href="/Contact">
+        Contact
+      </HoveredLink>
+    </Menu>
   )
 }
