fdfc6e1dbf2e7fb6cbac2deec49748340375c495
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-28 17:13:56 +0200

    fixed build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,16 @@ import { SparklesCore } from "@/components/aceternity/sparkles"
 import { ParallaxSection } from "@/components/photo-section"
 import { useScroll } from "framer-motion"
 import SiteLoad from "@/components/general/site-load"
-import { Navbar } from "@/components/general/navbar"
 import { ModeToggle } from "@/components/ui/mode-toggle"
 import Hero from "@/components/general/hero"
-import { FloatingNav } from "@/components/aceternity/floating-navbar"
+import { FloatingNav } from "@/components/general/floating-navbar"
 import { HomeIcon } from "lucide-react"
 import Grid from "@/components/general/grid"
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <FloatingNav
-        navItems={[{ name: "Home", link: "/", icon: <HomeIcon /> }]}
-      />
+      <FloatingNav />
       <Hero />
       <Grid />
     </main>
