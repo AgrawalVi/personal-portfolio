c2a441080fd50f20ca004f8770e1186a5f925ab6
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-28 14:10:40 -0500

    added styles

--- src/components/general/hero.tsx
+++ src/components/general/hero.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion"
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-36 h-screen w-screen">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -23,7 +23,7 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
 
-      <div className="flex justify-center relative my-20 z-10">
+      <div className="flex justify-center relative my-20 z-10 align-middle h-full pb-64 border-b-2 border-neutral-800/50">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
@@ -50,7 +50,7 @@ const Hero = () => {
             }}
             className="md:mt-10"
           >
-            <a href="#about">
+            <a href="/#projects">
               <MagicButton
                 icon={<Folders />}
                 position="right"
