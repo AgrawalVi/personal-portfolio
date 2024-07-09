5a85439ea5f513e550058c36b374f3e50ec8503f
Vishrut Agrawal <vagrawal24@wisc.edu>
2024-07-09 14:39:04 -0500

    Working on project page layout

---
+++ src/app/projects/sweetbeasts/page.tsx
@@ -0,0 +1,12 @@
+export default function SweetBeastsPage() {
+  return (
+    <main className="flex w-full justify-center">
+      <div className="max-w-4xl w-full flex flex-col items-center justify-center">
+        <h1 className="w-full text-center">SweetBeasts</h1>
+        <div className="flex flex-col py-10 w-full">
+          <p>hello</p>
+        </div>
+      </div>
+    </main>
+  )
+}
