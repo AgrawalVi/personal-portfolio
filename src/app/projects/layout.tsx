e76080d028ccdca7d67f30b5251f80664271766f
Vishrut Agrawal <126030257+AgrawalVi@users.noreply.github.com>
2024-07-11 02:02:29 -0500

    Update layout.tsx

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -6,6 +6,6 @@ export default function projectsLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="h-full w-full px-8 min-[370px]:px-16 pb-16">{children}</div>
+    <div className="h-full w-full px-8 min-[370px]:px-16 pt-5 sm:pt-10 pb-16">{children}</div>
   )
 }
