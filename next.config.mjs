4f5c8ee13b0d609093071ed6e2d30b44ba0ecb28
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-14 12:53:33 -0500

    setup velite

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,29 @@
-import nextMdx from '@next/mdx'
-
-const withMdx = nextMdx({
-  // By default only the `.mdx` extension is supported.
-  extension: /\.mdx?$/,
-  options: {
-    /* otherOptions… */
-  },
-})
+import { build } from 'velite'
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      await build({ watch: dev, clean: !dev })
+    })
+  }
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['mdx', 'ts', 'tsx'],
   images: {
     domains: [
       'api.microlink.io', // Microlink Image Preview
     ],
   },
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
+  },
 }
 
-export default withMdx(nextConfig)
+export default nextConfig
