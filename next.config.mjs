be7392ee9156e0753865c637972673cd9c769ea2
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-14 13:12:13 -0500

    changed next.config.mjs to use turbopack velite config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
-import { build } from 'velite'
-class VeliteWebpackPlugin {
-  static started = false
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      await build({ watch: dev, clean: !dev })
-    })
-  }
+const isDev = process.argv.indexOf('dev') !== -1
+const isBuild = process.argv.indexOf('build') !== -1
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = '1'
+  const { build } = await import('velite')
+  await build({ watch: isDev, clean: !isDev })
 }
 
 /** @type {import('next').NextConfig} */
@@ -19,10 +12,6 @@ const nextConfig = {
     domains: [
       'api.microlink.io', // Microlink Image Preview
     ],
-  },
-  webpack: (config) => {
-    config.plugins.push(new VeliteWebpackPlugin())
-    return config
   },
 }
 
