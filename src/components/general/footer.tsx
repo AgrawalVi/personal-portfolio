4912f7caa8b22c99637a7ea1b3bad31fe7b18b66
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-14 12:57:44 -0500

    changed border color

--- src/components/general/footer.tsx
+++ src/components/general/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="sm:h-20 flex justify-center sm:justify-end text-xs sm:text-base border-t border-border/30 text-muted-foreground p-3 sm:p-5 items-end sm:items-center sm:px-20">
+    <div className="sm:h-20 flex justify-center sm:justify-end text-xs sm:text-base border-t border-border/50 text-muted-foreground p-3 sm:p-5 items-end sm:items-center sm:px-20">
       <div>©2024 Vishrut Agrawal</div>
     </div>
   )
