33048eea90bbe2fa66e5fbe63cc305d43dae75de
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-14 21:23:52 -0500

    Added styling for the blog pages

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -3,6 +3,8 @@ import { MDXContent } from '@/components/mdx-components'
 import { notFound } from 'next/navigation'
 
 import '@/styes/mdx.css'
+import { TracingBeam } from '@/components/aceternity/tracing-beam'
+import { formatDate } from '@/lib/utils'
 
 interface PostPageProps {
   params: {
@@ -33,9 +35,20 @@ export default async function postPage({ params }: PostPageProps) {
   }
 
   return (
-    <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
-      <h1 className="mb-2">{post.title}</h1>
-      <MDXContent code={post.body} />
-    </article>
+    <main>
+      <article className="container py-6 max-w-5xl mx-auto pt-10 sm:pt-20">
+        <TracingBeam>
+          <div className="mb-12 w-full border-b">
+            <h1 className="text-center mb-0">{post.title}</h1>
+            <p className="text-center opacity-80 pb-5">{post.description}</p>
+            <div className="flex justify-between opacity-80 text-sm pb-1">
+              <p>Written by Vishrut Agrawal</p>
+              <p>{formatDate(post.date)}</p>
+            </div>
+          </div>
+          <MDXContent code={post.body} />
+        </TracingBeam>
+      </article>
+    </main>
   )
 }
