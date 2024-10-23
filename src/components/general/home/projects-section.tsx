7f31b0188fcf5ec12604a2a0f3fad89bfcea21aa
Vishrut Agrawal <126030257+AgrawalVi@users.noreply.github.com>
2024-10-23 15:27:57 -0500

    Updated Sweetbeasts website URL

--- src/components/general/home/projects-section.tsx
+++ src/components/general/home/projects-section.tsx
@@ -37,8 +37,8 @@ export default function ProjectSection() {
           timeLine="May 2024 - Present"
           bulletPoints={SweetBeastsCardDescription}
           technologies={SweetBeastsTechnologies}
-          liveLink="https://sweetbeasts-demo.vishrut.tech"
-          githubLink="https://github.com/AgrawalVi/SweetBeasts/tree/testing"
+          liveLink="https://sweetbeasts.shop"
+          githubLink="https://github.com/AgrawalVi/SweetBeasts"
         />
         <ProjectCardContainer
           title="SweetBeasts Admin"
