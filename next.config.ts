9bd7fdf38c5ae152a875d070c10efc6f666fae93
Vishrut Agrawal <zvishrut@gmail.com>
2026-05-03 18:28:18 -0500

    Added routes from old portfolio

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,35 @@ import createMDX from '@next/mdx'
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/ponovo',
+        destination: 'https://ponovo.app',
+        permanent: false
+      },
+      {
+        source: '/tanks',
+        destination: 'https://www.youtube.com/watch?v=w69SsBIJABA',
+        permanent: false
+      },
+      {
+        source: '/sweetbeasts',
+        destination: 'https://sweetbeasts.shop',
+        permanent: false
+      },
+      {
+        source: '/resume',
+        destination: 'https://drive.google.com/file/d/1_euc4NMr6xQrrPhRQh94nQ2NGSiAIXeo/view?usp=drivesdk',
+        permanent: false
+      },
+      {
+        source: '/resume/mba',
+        destination: 'https://drive.google.com/file/d/1rk2hhINRasavxC8V0s4WG8WhE9-A2lpU/view?usp=sharing',
+        permanent: false
+      }
+    ]
+  }
 }
 
 const withMDX = createMDX({
